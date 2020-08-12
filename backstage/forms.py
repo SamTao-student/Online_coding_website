481096cfaee01787c490f6ea58d01386efe5dc5e
Xiahaitao <1662146384@qq.com>
2020-08-12 15:49:38 +0800

    2020.8.12,15:48

--- backstage/forms.py
+++ backstage/forms.py
@@ -5,9 +5,9 @@
 # @Software: PyCharm
 from django import forms
 from djangocodemirror.fields import CodeMirrorField
-from djangocodemirror import widgets
+
 class CreateTask(forms.Form):
     title = forms.CharField()
-    descriptions = forms.CharField()
+    descriptions = forms.CharField(widget=forms.Textarea)
     scripts = CodeMirrorField(label="代码", required=True,
                           config_name="x-c",)
